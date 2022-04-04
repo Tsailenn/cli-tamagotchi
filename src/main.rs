c82c67983e54cc837870bbc64dc4efe6ea4e2432
Tsailenn <70084579+Tsailenn@users.noreply.github.com>
2022-04-04 16:59:02 -0700

    init

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod tamagotchi;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/tamagotchi.rs
@@ -0,0 +1,25 @@
+#[derive(Debug, Copy, Clone)]
+struct Tamagotchi {
+    display: [[usize; 8]; 8],
+    hunger: usize,
+    health: usize,
+    poisoning: usize,
+}
+
+impl Tamagotchi {
+    fn update(&mut self) {
+        todo!()
+    }
+
+    fn feed(&mut self) {
+        todo!()
+    }
+
+    fn cure(&mut self) {
+        todo!()
+    }
+
+    fn draw(&mut self) {
+        todo!()
+    }
+}
